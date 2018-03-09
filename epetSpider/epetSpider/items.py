ca764aa058698ede7c92ff7594a5ce506c35f1ff
Sven Wong <illust@users.noreply.github.com>
2018-03-09 19:12:20 +0800

    add work

---
+++ epetSpider/epetSpider/items.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import scrapy
+from scrapy import Field,Item
+
+class epetSpiderItem(Item):
+    
+    skuUrl = scrapy.Field()     # 商品url
+    skuCtgs = scrapy.Field()	# 存储分类信息
+    skuTitle = scrapy.Field()	# 商品名称
+    skuSubTitle = scrapy.Field()# 商品子标题
+    skuPrice = scrapy.Field()	# 商品价格
+    # skuParas = scrapy.Field()	# 商品其他参数
+
